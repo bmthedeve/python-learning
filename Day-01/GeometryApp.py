147bb83ad4e81fadbbae0f5ac00a7a9ab3d8127c
balamurugan <bmthedev@gmail.com>
2024-04-11 18:50:17 +0530

    added Constructor

---
+++ Day-01/GeometryApp.py
@@ -0,0 +1,14 @@
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        print(self.x, self.y)
+
+    # def falls_in_rectangle(self,point):
+        # if self.x < point.x && self.y < point.y :
+        #     return True
+        # else:
+        #     return False
+
+point1 = Point(5,6)
